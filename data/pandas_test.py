d85b97a18d9eaff0eeb736ea65c9887b038f7619
Brandon <brandonkemboi87@gmail.com>
2025-12-23 14:42:50 -0600

    pandas practice

--- data/pandas_test.py
+++ data/pandas_test.py
@@ -48,8 +48,62 @@ df = pd.concat([df, new_row])
 print(df)
 print()
 ############### Importing ###############
-packer_df = pd.read_csv("data/data.csv")
+
+packer_df = pd.read_csv("data/data.csv", index_col="week")
 print(packer_df)
 print()
 print(packer_df.loc[5])
-    
+
+############### Selection ###############
+# Selection by Column
+print()
+print(packer_df["home_team"])
+print(packer_df[["home_team", "away_team", "gameday"]])
+print()
+# Selection by Row
+print(packer_df.loc[1])
+print(packer_df.loc[1:20, ["home_team", "away_team", "packers_result"]])
+print()
+# week_result = int(input('Which week result do you want to see?'))
+
+# try:
+#     print(packer_df.loc[week_result])
+# except KeyError:
+#     print(f'{week_result} not found')
+
+print()
+############### Filtering ###############
+
+# Filtering = Keeping the rows that match a condition
+
+wins_packers = packer_df[packer_df["packers_result"]== "W"]
+print(wins_packers)
+
+packers_score = packer_df[(packer_df["away_team"] == "GB") & 
+                          (packer_df["packers_result"] == "W")]
+
+print(packers_score)
+print()
+############### Aggreation ###############
+
+# Aggregate functions = Reduces a set of values into a single summary value. Used to Summarize and analyze data. Often used with groupby() function
+
+print(packer_df.mean(numeric_only = True))
+
+print(packer_df["away_score"].mean())
+
+group = packer_df.groupby("home_team")
+
+print (group["home_score"].mean())
+print()
+############### Data Cleaning ###############
+
+# Data Cleaning = The process of fixing/removing: Incomplete, incorrect, or irrelevant data. 75% of work done with Pandas is data cleaning
+
+print(packer_df)
+packer_df = packer_df.drop(columns = ["gameday"])
+packer_df['home_team'] = packer_df['home_team'].replace({"GB": "Packers Home"})
+packer_df['away_team'] = packer_df['away_team'].replace({"GB": "Packers Away"})
+packer_df['packers_result'] = packer_df['packers_result'].replace({"W": "Win", "L": "Lost"})
+
+print(packer_df)
